96bcff683647afeb62fd7be435147b9a43dd1e43
wazy <biobud74@yahoo.com>
2014-02-24 00:46:13 -0500

    More work on encryption

--- src/edu/uncfsu/csc490/Encryption.java
+++ src/edu/uncfsu/csc490/Encryption.java
@@ -1,36 +1,85 @@
 package edu.uncfsu.csc490;
+
 import java.util.Scanner;
 
-
 public class Encryption {
+
+	private static void printMatrix(int[][] matrix) {
+		for (int i = 0; i < matrix.length; i++) {
+			for (int j = 0; j < matrix[0].length; j++) {
+				System.out.print(matrix[i][j] + " ");
+			}
+			System.out.println();
+		}
+	}
+	
+	public static int[][] matrixMultiply(int A[][], int B[][]) {
+		int[][] C = new int[2][1];
+		for (int i = 0; i < 2; i++) { // aRow
+			for (int j = 0; j < 2; j++) { // bColumn
+				for (int k = 0; k < 1; k++) { // aColumn
+					C[i][j] = A[i][k] * B[k][j];
+				}
+			}
+		}
+
+		return C;
+	}
 
 	public static void main(String[] args) {
 		//user inputs message as string
 		System.out.println("Enter your message");
-		Scanner input = new Scanner (System.in);
-		String message = input.nextLine();
-		String message1 = message.toLowerCase();
 		
+		Scanner input = new Scanner (System.in);
 		
 		//put message into character array
-		char [] array1 = message1.toCharArray();
-		
-		if ((array1.length & 1) == 1){
-			System.out.println("odd");
-			char temp = array1[array1.length - 1];
-		}	
-	
-		for (int i = 0; i < array1.length; i++) {
+		char [] array1 = input.nextLine().toCharArray();
 
-			char c = array1[i];
+		int n = (int) Math.ceil(array1.length / 2.0);
+		
+		int[][] matrix = new int[2][n];
+
+		// store chars in 2X1 vectors
+		for (int i = 0; i < matrix[0].length; i++) {
+			int index = i * 2;
+			matrix[0][i] = array1[index];
 			
-			System.out.println(c % 26);
+			// if odd re-copy last letter
+			if ((index == array1.length - 1) && (array1.length % 2) == 1) {
+				matrix[1][i] = array1[index];
+			}
+			else {
+				matrix[1][i] = array1[index+1];
+			}
 		}
 		
+		int[][] a = new int[2][2];
 		
-		//put message into groups of 2 chars, duplicate last char if odd # of chars in message
+		a[0][0] = 1;
+		a[0][1] = 1;
+		a[1][0] = 0;
+		a[1][1] = 3;
+
+		int[][] temp = new int[2][1];
 		
-		//switch chars in each group to their int value and store in 2X1 vectors
+		printMatrix(matrix);
+		
+		for (int i = 0; i < matrix[0].length-1; i++) {
+			int index = i * 2;
+			temp[0][0] = matrix[0][index];
+			temp[1][0] = matrix[1][index+1];
+			
+			temp = matrixMultiply(temp, a);
+			
+			matrix[0][index] = temp[0][0];
+			matrix[1][index+1] = temp[1][0];
+			
+		}
+		
+		System.out.println();
+		
+		printMatrix(matrix);
+		//printMatrix(matrix1);
 		
 		//multiply each vector individually with a matrix A and store in new 2X1 vector
 		
@@ -41,8 +90,6 @@ public class Encryption {
 		//combine the new char groups into one string
 		
 		//print the new encrypted message
-		
-			
 		
 		//}
 		input.close();
